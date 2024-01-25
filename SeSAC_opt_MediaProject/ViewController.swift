a2eaefcaa65a2539cc1b1ece5a99f3675a47bd8f
jieun <jieun_0330@naver.com>
2024-01-25 20:44:05 +0900

    :tada: [init] initial commit

---
+++ SeSAC_opt_MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_opt_MediaProject
+//
+//  Created by 박지은 on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
